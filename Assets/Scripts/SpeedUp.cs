fabb90e40179d96e5177b84a2599a9de871e3502
Paul Turaczyk <kantal.streamy@gmail.com>
2019-10-13 22:03:16 +0200

    Version 0.57
    Speed buttons

---
+++ Assets/Scripts/SpeedUp.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUp : MonoBehaviour
+{
+
+    public void Speed2x()
+    {
+        Time.timeScale = 2f; 
+    }
+
+    public void Speed1x()
+    {
+        Time.timeScale = 1f;
+    }
+}
